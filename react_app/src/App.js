b21caa48e58544e0192f40c81efb1130fb5dda6e
Yuki Tomioka <yuki.tomioka@zozo.com>
2020-11-30 00:22:24 +0900

    create Component include form

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -12,6 +12,11 @@ class App extends Component {
     padding: "5px",
   }
 
+  inputStyle = {
+    fontSize: "12pt",
+    padding: "5px",
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -36,41 +41,14 @@ class App extends Component {
     return (
       <div>
         <h1>React</h1>
-        <Message title="Children!">
-          これはコンポーネント内のコンテンツです。
-          マルでテキストを分割し、リストにして表示します。
-          改行は必要ありません。
-        </Message>
-      </div>
-    )
-  }
-}
-
-class Message extends Component {
-  li = {
-    listStyleType: "square",
-    fontSize: "16pt",
-    color: "#06",
-    margin: "0px",
-    padding: "0px",
-  }
-
-  render() {
-    let content = this.props.children;
-    let arr = content.split("。");
-    let arr2 = [];
-    for(let i = 0;i < arr.length;i++) {
-      if (arr[i].trim() !== "") {
-        arr2.push(arr[i]);
-      }
-    }
-    let list = arr2.map((value, key)=>(
-      <li style={this.li} key={key}>{value}</li>
-    ));
-    return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <ol>{list}</ol>
+        <h2>{this.state.message}</h2>
+        <form onSubmit={this.doSubmit}>
+          <label>
+            <span style={this.inputStyle}></span>Message:
+            <input type="text" style={this.inputStyle} onChange={this.doChange} />
+          </label>
+          <input type="submit" style={this.inputStyle} value="Click" />
+        </form>
       </div>
     )
   }
