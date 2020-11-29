dec1a649f3350fdfc80451635dc06ddf73e215c4
Yuki Tomioka <yuki.tomioka@zozo.com>
2020-11-29 20:03:04 +0900

    change style by update state

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,7 +3,7 @@ import Rect from "./React";
 import './App.css';
 
 class App extends Component {
-  msgStyle = {
+  msgStyle1 = {
     fontSize: "24pt",
     color: "#900",
     margin: "20px 0px",
@@ -11,9 +11,18 @@ class App extends Component {
     borderBottom: "2px solid #900"
   }
 
+  msgStyle2 = {
+    fontSize: "20pt",
+    color: "white",
+    backgroundColor: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  }
+
   btnStyle = {
-    fontSize: "20ot",
-    padding: "0px 10px"
+    fontSize: "16pt",
+    padding: "10px"
   }
 
   constructor(props) {
@@ -21,6 +30,7 @@ class App extends Component {
     this.state = {
       counter: 0,
       msg: "count start!",
+      flg: true,
     };
     this.doAction = this.doAction.bind(this);
   }
@@ -28,14 +38,19 @@ class App extends Component {
   doAction(e) {
     this.setState((state) => ({
       counter: state.counter + 1,
-      msg: "count: " + state.counter
+      msg: "count: " + state.counter,
+      flg: !state.flg,
     }));
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      <p style={this.msgStyle}>{this.state.msg}</p>
+      {this.state.flg ?
+        <p style={this.msgStyle1}>count: {this.state.msg}</p>
+      :
+        <p style={this.msgStyle2}>{this.state.msg}です。</p>
+      }
       <button style={this.btnStyle} onClick={this.doAction}>
         Click
       </button>
