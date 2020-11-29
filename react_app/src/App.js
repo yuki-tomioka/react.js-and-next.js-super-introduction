f628264ecc1dc8013f2a8122d61802ad491abf37
Yuki Tomioka <yuki.tomioka@zozo.com>
2020-11-29 20:55:31 +0900

    cooperation of state and propaty

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,57 +3,57 @@ import Rect from "./React";
 import './App.css';
 
 class App extends Component {
-  msgStyle1 = {
-    fontSize: "24pt",
+  data = [];
+
+  msgStyle = {
+    fontSize: "20pt",
     color: "#900",
     margin: "20px 0px",
     padding: "5px",
-    borderBottom: "2px solid #900"
   }
 
-  msgStyle2 = {
-    fontSize: "20pt",
-    color: "white",
-    backgroundColor: "#900",
-    margin: "20px 0px",
-    padding: "5px",
-    borderBottom: "2px solid #900"
-  }
-
-  btnStyle = {
-    fontSize: "16pt",
-    padding: "10px"
+  area = {
+    width: "500px",
+    height: "500px",
+    border: "1px solid blue",
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
-      msg: "count start!",
-      flg: true,
+      list: this.data
     };
     this.doAction = this.doAction.bind(this);
   }
 
   doAction(e) {
-    this.setState((state) => ({
-      counter: state.counter + 1,
-      msg: "count: " + state.counter,
-      flg: !state.flg,
-    }));
+    let x = e.pageX;
+    let y = e.pageY;
+    this.data.push({x:x, y:y});
+    this.setState({
+      list: this.data
+    });
+  }
+
+  draw(d) {
+    let s = {
+      position: "absolute",
+      left: (d.x - 25) + "px",
+      top: (d.y - 25) + "px",
+      width: "50px",
+      height: "50px",
+      backgroundColor: "#66f3",
+    };
+    return <div style={s}></div>;
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      {this.state.flg ?
-        <p style={this.msgStyle1}>count: {this.state.msg}</p>
-      :
-        <p style={this.msgStyle2}>{this.state.msg}です。</p>
-      }
-      <button style={this.btnStyle} onClick={this.doAction}>
-        Click
-      </button>
+      <h2 style={this.msgStyle}>show rect.</h2>
+      <div style={this.area} onClick={this.doAction}>
+        {this.data.map((value)=>this.draw(value))}
+      </div>
     </div>;
   }
 }
