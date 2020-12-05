5c0431a7f114de2c7fd3e2bcdd57f423a782290b
Yuki Tomioka <yuki.tomioka@zozo.com>
2020-12-06 06:32:20 +0900

    create component with redux

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from "redux";
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
+// ステートの値
+let state_value = {
+  counter: 0,
+  message: "COUNTER",
+}
+
+// レデューサー
+function counter (state = state_value, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+        message: "INCREMENT",
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+        message: "DECREMENT",
+      };
+    default:
+      return state;
+  }
+}
+
+// ストアを作成
+let store = createStore(counter);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
